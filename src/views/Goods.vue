e4306c627ff79ccf061c2152fde933779bb194df
Sergey Mokhonko <sergiy35@gmail.com>
2019-10-03 21:58:02 +0300

    Fixed views page of goods. Added date field to the page.

--- src/views/Goods.vue
+++ src/views/Goods.vue
@@ -72,14 +72,39 @@
                 ></v-select>
             </div>
             <div class="box">
+                <v-menu
+                    v-model="menu2"
+                    :close-on-content-click="false"
+                    :nudge-right="40"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="290px"
+                >
+                    <template v-slot:activator="{ on }">
+                    <v-text-field
+                        color = '#3498db'
+                        v-model="selectedDate"
+                        label="Оберіть дату:"
+                        prepend-icon="event"
+                        readonly
+                        v-on="on"
+                    ></v-text-field>
+                    </template>
+                    <v-date-picker v-model="selectedDate" @input="menu2 = false"></v-date-picker>
+                </v-menu>
+            </div>
+            <div class="box">
                 <v-textarea
                     outlined
                     name="Comment"
                     label="Коментар"
                 ></v-textarea>
             </div>
+            <div class="box" style="margin-bottom: 15px;">
+                <h1>Сума: {{ price }} грн.</h1>
+            </div>
             <div class="box">
-                <h1>Сумма: {{ price }} грн.</h1>
+                <v-btn color='primary' large>Додати у кошик</v-btn>
             </div>
         </div>
     </div>
@@ -94,6 +119,7 @@ export default {
         selectedFill: null,
         selectedCream: null,
         selectedDecor: null,
+        selectedDate: new Date().toISOString().substr(0, 10),
         goodsById: {},
     }),
     computed: {
@@ -124,9 +150,16 @@ export default {
 </script>
 
 <style scoped>
+@media screen and (max-width: 860px) {
+    .container {
+        flex-direction: column;
+    }
+}
+
 .container {
     max-width: 100%;
     min-width: 350px;
+    margin: 40px 0;
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
@@ -148,8 +181,7 @@ export default {
 
 .price {
     padding: 10px;
-    border: 2px solid red;
-    background: #ccc;
     border-radius: 5px;
+    color: #3498db;
 }
 </style>
