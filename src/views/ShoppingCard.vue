2396844ef0fa56d497eef607ef98690322ccb5c9
Sergey Mokhonko <sergiy35@gmail.com>
2019-10-04 20:26:20 +0300

    Edited page Shopping Card

--- src/views/ShoppingCard.vue
+++ src/views/ShoppingCard.vue
@@ -1,132 +1,72 @@
 <template>
-    <v-container fluid >
-        <v-row no-gutters>
-            <v-col sm="6" xs="12">
-                <v-col v-for="item in 1" :key="item">
-                    <v-card
-                        class="mx-auto"
-                        max-width="350"
-                    >
-                        <v-img
-                        class="white--text"
-                        height="200px"
-                        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                        >
-                        <v-card-title class="align-end fill-height">Top 10 Australian beaches</v-card-title>
-                        </v-img>
-
-                        <v-card-text>
-                        <span>Number 10</span><br>
-                        <span class="text--primary">
-                            <span>Whitehaven Beach</span><br>
-                            <span>Whitsunday Island, Whitsunday Islands</span>
-                        </span>
-                        </v-card-text>
-
-                        <v-card-actions>
-                        <v-btn
-                            color="success"
-                        >
-                            Read More
-                        </v-btn>
-                        <v-btn
-                            color="error"
-                        >
-                            Delete
-                        </v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-col>
-            </v-col>
-            <v-col sm="6" xs="12">
-                <h1>Information about you</h1>
-                <form>
-                    <v-text-field
-                        v-model="name"
-                        label="Your Name *"
-                    ></v-text-field>
-                    <v-text-field
-                        v-model="phone"
-                        label="Phone number *"
-                    ></v-text-field>
-                    <v-text-field
-                        v-model="comment"
-                        label="Comment"
-                    ></v-text-field>
-                    <v-text-field
-                        v-model="socialpage"
-                        label="Social page"
-                    ></v-text-field>
-
-                    <v-sheet height="500" class="mb-4">
-                        <v-calendar
-                            type="month"
-                            now="2019-01-08"
-                            value="2019-01-08"
-                            :events="events"
-                        ></v-calendar>
-                    </v-sheet>
-                    
-                    <v-btn color="success" class="mr-4" @click="submit">Buy</v-btn>
-                    <v-btn color='error' @click="clear">clear</v-btn>
-
-                </form>
-
-            </v-col>
-        </v-row>
-    </v-container>
+  <v-container fluid>
+    <v-row no-gutters>
+      <v-col sm="6" xs="12">
+        <v-card class="mr-3" max-width="100%" min-width="350px">
+          <v-list max-width="100%">
+            <v-list-item v-for="item in 5" :key="item">
+              <v-list-item-avatar size="60">
+                <v-img src="https://picsum.photos/id/106/2592/1728"></v-img>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title v-text="title"></v-list-item-title>
+                <v-list-item-subtitle three-line v-html="content"></v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn icon>
+                  <v-icon color="error">delete_forever</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+      <v-col sm="6" xs="12">
+        <h1>Трохи інформації про Вас</h1>
+        <form>
+          <v-text-field v-model="name" label="Ваше ім'я *"></v-text-field>
+          <v-text-field v-model="phone" label="Номер телефона *"></v-text-field>
+          <v-text-field v-model="comment" label="Коментар"></v-text-field>
+          <v-text-field v-model="socialpage" label="Посилання на соціальні мережі"></v-text-field>
+          <v-btn color="success" class="mr-4" @click="submit">Замовити</v-btn>
+          <v-btn color="error" @click="clear">Очистити</v-btn>
+        </form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  export default {
-    data () {
-        return {
-            name: '',
-            phone: '',
-            coment: '',
-            socialpage: '',
-            items: [
-                { text: 'Real-Time', icon: 'mdi-clock' },
-                { text: 'Audience', icon: 'mdi-account' },
-                { text: 'Conversions', icon: 'mdi-flag' },
-            ],
-            events: [
-                {
-                    name: 'Vacation',
-                    start: '2018-12-30',
-                    end: '2019-01-02',
-                },
-                {
-                    name: 'Meeting',
-                    start: '2019-01-07',
-                },
-                {
-                    name: '30th Birthday',
-                    start: '2019-01-03',
-                },
-                {
-                    name: 'New Year',
-                    start: '2019-01-01',
-                },
-                {
-                    name: 'Conference',
-                    start: '2019-01-21',
-                },
-                {
-                    name: 'Hackathon',
-                    start: '2019-01-30',
-                    end: '2019-02-01',
-                },
-            ],
-        }
-    },
-    methods: {
-        clear () {
-            this.name = ''
-            this.phone = ''
-            this.coment = ''
-            this.socialpage = ''
-        }
+export default {
+  data() {
+    return {
+      name: "",
+      phone: "",
+      comment: "",
+      socialpage: "",
+      title: "Tittle",
+      content:
+        "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?",
+      items: [
+        { text: "Real-Time", icon: "mdi-clock" },
+        { text: "Audience", icon: "mdi-account" },
+        { text: "Conversions", icon: "mdi-flag" }
+      ]
+    };
+  },
+  methods: {
+    clear() {
+      this.name = "";
+      this.phone = "";
+      this.comment = "";
+      this.socialpage = "";
     }
   }
+};
 </script>
+
+<style scoped>
+.container {
+  margin-top: 40px;
+}
+</style>
