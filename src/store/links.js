3033ebd0a430ba617fc3f6ee3a4e54568cdd4b5b
Sergey Mokhonko <sergiy35@gmail.com>
2019-10-14 19:59:24 +0300

    rename links

--- src/store/links.js
+++ src/store/links.js
@@ -1,13 +1,13 @@
 export default {
     state: {
         links: [
-            { href: '/', name: 'Home', icon: 'home' },
-            { href: '/about', name: 'About', icon: 'person' },
-            { href: '/contacts', name: 'Contact me', icon: 'contacts' },
-            { href: '/shopping-card', name: 'Card', icon: 'shopping_cart' },
-            { href: '/Login', name: 'Login', icon: 'vpn_key' },
-            { href: '/admin/add-new-goods', name: 'New Goods', icon: 'fiber_new' },
-            { href: '/admin/goods', name: 'List of Goods', icon: 'list' },
+            { href: '/', name: 'Головна', icon: 'home' },
+            { href: '/about', name: 'Про нас', icon: 'person' },
+            { href: '/contacts', name: 'Контакти', icon: 'contacts' },
+            { href: '/shopping-card', name: 'Кошик', icon: 'shopping_cart' },
+            { href: '/Login', name: 'Вхід', icon: 'vpn_key' },
+            { href: '/admin/add-new-goods', name: 'Додати товар', icon: 'fiber_new' },
+            { href: '/admin/goods', name: 'Всі продукти', icon: 'list' },
         ]
     },
     mutation: {
